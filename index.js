92772d9e43449050a611c9e844bc81d324cf9429
mohammedkhalf <mohammedkhalf.2022@gmail.com>
2021-10-18 12:28:11 +0200

    create module & export it

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
 
-// const folderPath = "C:\\Users\\TW\\Desktop\\Meetup";
+var user = require('./user');
 
-// fs.readdir(folderPath , (err , files)=> {
-// 	// console.log("files : " + files);
-// 	files.forEach((filename, index) => {
-// 		console.log("filename : " + filename);
-// 	})
-// })
-
-const filePath = "C:\\Users\\TW\\Desktop\\Meetup\\package.json";
-var content = fs.readFileSync(filePath);
-console.log("content is : " + content);
+console.log("username is : " + user.getUserName());
+console.log("email is : " + user.getEmail());
---
+++ user.js
@@ -0,0 +1,10 @@
+function getUserName(){
+    return "ahmed";
+}
+
+function getEmail(){
+    return "mohamedkhalf@gmail.com"
+}
+
+exports.getUserName = getUserName;
+exports.getEmail = getEmail;
